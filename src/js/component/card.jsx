28626aa375d5a37834abc851cba13d2c226f597b
Jess <hi@jesstechpreneur.com>
2022-05-28 13:58:10 +0000

    Project files

---
+++ src/js/component/card.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+//create your first component
+export const Cards = () => {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="card col-md-3">
+					<img
+						src="https://picsum.photos/500/320
+"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<a
+							className="btn btn-primary btn-lg"
+							href="#"
+							role="button">
+							Learn more
+						</a>
+					</div>
+				</div>
+				<div className="card col-md-3">
+					<img
+						src="https://picsum.photos/500/320
+"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<a
+							className="btn btn-primary btn-lg"
+							href="#"
+							role="button">
+							Learn more
+						</a>
+					</div>
+				</div>
+				<div className="card col-md-3">
+					<img
+						src="https://picsum.photos/500/320
+"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<a
+							className="btn btn-primary btn-lg"
+							href="#"
+							role="button">
+							Learn more
+						</a>
+					</div>
+				</div>
+				<div className="card col-md-3">
+					<img
+						src="https://picsum.photos/500/320
+"
+						className="card-img-top"
+						alt="..."
+					/>{" "}
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This content
+							is a little bit longer.
+						</p>
+						<a
+							className="btn btn-primary btn-lg"
+							href="#"
+							role="button">
+							Learn more
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Cards } from "../component/card.jsx";
+import { Navbar } from "../component/navbar.jsx";
+import { Jumbotron } from "../component/jumbotron.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,18 +10,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<Navbar />
+				<Jumbotron />
+				<Cards />
+			</div>
 		</div>
 	);
 };
